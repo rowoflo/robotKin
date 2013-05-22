114ba43545f493bbeddb786d6d1cb923ddf5f3c6
Rowland O'Flaherty <rowland.oflaherty@gmail.com>
2013-05-22 18:55:15 -0400

    Initial Commit

---
+++ robotKin/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  robotKin
+//
+//  Created by Rowland O'Flaherty on 5/22/13.
+//  Copyright (c) 2013 Rowland O'Flaherty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
